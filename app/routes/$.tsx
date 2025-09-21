ece5ead86e6f6a0be8ed281be828b591e152e597
brianbancroft <hello@brianbancroft.ca>
2025-09-20 21:41:20 -0700

    Add catchall

---
+++ app/routes/$.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "react-router";
+
+export async function clientLoader({ request }: { request: Request }) {
+  const url = new URL(request.url);
+  
+  // If someone tries to access /index.html, redirect to home
+  if (url.pathname === "/index.html") {
+    throw redirect("/", { status: 301 });
+  }
+  
+  // For any other unmatched routes, show 404
+  throw new Response("Not Found", { status: 404 });
+}
+
+export default function CatchAll() {
+  return null;
+}
